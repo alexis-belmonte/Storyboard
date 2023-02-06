98c41853289da188b118e1fe07ca2a7d5e7ab607
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-06 23:54:02 +0100

    brand-logo: add initial implementation

---
+++ src/assets/brand/BrandLogo.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div>storyboard</div>
+</template>
+
+<style scoped>
+div {
+    font-family: 'Archivo Black';
+
+    text-decoration: none;
+
+    color: var(--sb-primary-color);
+}
+</style>
