3f951443d9a1a8090571feffccd9504e27ac9547
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-07 16:23:45 +0100

    components.ui.page-container: add initial implementation

---
+++ src/components/app/ui/PageContainer.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <div class="container">
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+.container {
+    padding: 0pt 24pt;
+}
+</style>
