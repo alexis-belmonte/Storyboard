32964af45658f9de19db0562aad0ed02c7d3873e
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-07 16:24:06 +0100

    components.app.ui.flow-container: add initial implementation

---
+++ src/components/app/ui/FlowContainer.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="container">
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: grid;
+
+    gap: 8pt;
+
+    grid-template-columns: repeat(auto-fit, minmax(300pt, 1fr));
+}
+
+</style>
