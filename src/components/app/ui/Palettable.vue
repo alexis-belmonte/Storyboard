247f312ebef0fb0b2458038dc49d87d339296e1d
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-08 10:20:22 +0100

    components.app.ui.generic.palettable: add initial implementation

---
+++ src/components/app/ui/Palettable.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+    props: {
+        "palette": String
+    },
+    computed: {
+        palettableStyle() {
+            return {
+                "--pt-color":      this.palette ? `var(--sb-${this.palette}-color)` : 'inherit',
+                "--pt-dark-color": this.palette ? `var(--sb-${this.palette}-dark-color)` : 'inherit'
+            }
+        }
+    }
+}
+</script>
