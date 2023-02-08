8e410a52609acc9f57b323de9a5eef47d66cb043
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-08 10:12:02 +0100

    components.app.menu: objectify the component

--- src/components/app/Menu.vue
+++ src/components/app/Menu.vue
@@ -1,32 +1,71 @@
-<script lang="ts" setup>
+<script lang="ts">
 import BrandLogo from '@/assets/brand/BrandLogo.vue';
 
 import Button from '@/components/app/ui/Button.vue'
 import ProfileFace from '@/components/app/ui/ProfileFace.vue'
+
+let mounted: boolean = false
+
+export default {
+    components: {
+        BrandLogo,
+
+        Button,
+        ProfileFace
+    },
+    mounted() {
+        mounted = true
+    }
+}
 </script>
 
 <template>
-    <header>
-        <div class="left">
-            <Button icon="add" palette="primary" />
+    <div class="margin" />
+    <div class="parent" ref="menu">
+        <div class="container">
+            <header>
+                <div class="left">
+                    <Button icon="add" palette="primary" />
+                </div>
+                <div class="center">
+                    <router-link to="/" class="brand-logo-link">
+                        <BrandLogo />
+                    </router-link>
+                </div>
+                <div class="right">
+                    <Button icon="logout" palette="quartenary" />
+                    <ProfileFace
+                        palette="primary"
+                        src="https://thispersondoesnotexist.com/image"
+                        hoverable />
+                </div>
+            </header>
         </div>
-        <div class="center">
-            <router-link to="/" class="brand-logo-link">
-                <BrandLogo />
-            </router-link>
-        </div>
-        <div class="right">
-            <Button icon="logout" palette="quartenary" />
-            <ProfileFace palette="primary" src="https://thispersondoesnotexist.com/image" />
-        </div>
-    </header>
+    </div>
 </template>
 
 <style scoped>
+.margin {
+    /* FIXME: Do a better calculation of the amount of space taken by the menu bar */
+    margin-top: calc(var(--sb-spacing) * 4.25);
+}
+
+.parent > .container {
+    position: fixed;
+
+    top: 0;
+    width: 100%;
+}
+
 header {
     display: flex;
 
-    padding: 16pt 24pt;
+    padding: var(--sb-spacing) calc(var(--sb-spacing) * 1.5);
+
+    background: linear-gradient(180deg,
+        var(--sb-light-color) 45%,
+        var(--sb-transparent-color) 100%
+    );
 }
 
 header > div {
