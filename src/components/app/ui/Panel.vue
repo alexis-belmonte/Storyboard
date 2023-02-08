65fb973a677833340ec6ebf7c9c7fda0cc75011e
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-08 10:20:56 +0100

    components.app.ui.panel: add initial implementation

---
+++ src/components/app/ui/Panel.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import Palettable from './Palettable.vue';
+
+export default {
+    extends: Palettable,
+    props: {
+        'filled': {
+            type: Boolean
+        }
+    }
+}
+</script>
+
+<template>
+    <div :style="palettableStyle" class="container">
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+.container {
+    padding: 24pt;
+
+    border-width: var(--sb-primary-border-width);
+    border-radius: var(--sb-primary-radius);
+
+    box-shadow: rgba(var(--pt-color), 0.3) 0 16pt 35pt 0;
+}
+</style>
