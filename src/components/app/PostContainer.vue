ca41ff3e09da28219f663e947903611f53d80f13
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-08 10:44:45 +0100

    components.app.post-container: add initial implementation

---
+++ src/components/app/PostContainer.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import Palettable from './ui/Palettable.vue'
+
+import FlowContainer from './ui/FlowContainer.vue'
+
+export default {
+    extends: Palettable,
+
+    components: {
+        Palettable,
+
+        FlowContainer
+    }
+}
+</script>
+
+<template>
+    <FlowContainer :style="palettableStyle" class="container">
+        <slot />
+    </FlowContainer>
+</template>
+
+<style scoped>
+.container > :deep(*) {
+    aspect-ratio: 1;
+}
+</style>
