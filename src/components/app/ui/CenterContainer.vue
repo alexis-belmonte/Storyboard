8e8959e005e31746ccf918968f6f237876eaf72b
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-10 01:03:39 +0100

    components.app.ui.center-container: add initial implementation

---
+++ src/components/app/ui/CenterContainer.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+export default {};
+</script>
+
+<template>
+    <div class="container">
+        <div class="object">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+
+    width: 100vw;
+    height: 100vh;
+
+    align-items: center;
+    justify-content: center;
+}
+
+.container > .object {
+    flex-grow: 0;
+    flex-shrink: 1;
+    flex-basis: auto;
+}
+</style>
