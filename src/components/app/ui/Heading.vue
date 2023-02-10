276f2927e887e441c88c85b4bba016983282c024
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-10 01:22:12 +0100

    components.app.ui.heading: add initial implementation

---
+++ src/components/app/ui/Heading.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import Palettable from './Palettable.vue';
+
+export default {
+    extends: Palettable,
+    props: {
+        size: {
+            type: String,
+            default: 'medium'
+        }
+    },
+    computed: {
+        componentStyle() {
+            return {
+                '--hd-size': `var(--sb-${this.size}-font-size)`
+            }
+        }
+    }
+};
+</script>
+
+<template>
+    <span class="container" :style="[palettableStyle, componentStyle]">
+        <slot />
+    </span>
+</template>
+
+<style scoped>
+.container {
+    font-family: var(--sb-branding-font);
+    font-size: var(--hd-size);
+
+    color: rgb(var(--pt-color));
+}
+</style>
