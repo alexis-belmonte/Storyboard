71990d4e56a841c8803e4628914ad4c5b6f252c1
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-13 00:01:49 +0100

    components.app.ui.grid-container: resize cards to `250pt`

--- src/components/app/ui/GridContainer.vue
+++ src/components/app/ui/GridContainer.vue
@@ -3,17 +3,17 @@ export default {};
 </script>
 
 <template>
-    <div class="container">
+    <div class="grid-container">
         <slot />
     </div>
 </template>
 
 <style scoped>
-.container {
+.grid-container {
     display: grid;
 
     gap: calc(var(--sb-spacing) * 2);
 
-    grid-template-columns: repeat(auto-fit, minmax(200pt, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(250pt, 1fr));
 }
 </style>
