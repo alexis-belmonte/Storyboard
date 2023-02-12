4092595665869a9050bee77f497167063ad8e41c
Alexis Belmonte <alexis.belmonte@epitech.eu>
2023-02-13 00:00:29 +0100

    components.app.ui: add initial implementation

---
+++ src/components/app/ui/Content.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import Palettable from './Palettable.vue';
+
+export default {
+    extends: Palettable
+}
+</script>
+
+<template>
+    <span class="content" :style="palettableStyle">
+        <slot />
+    </span>
+</template>
+
+<style scoped>
+.content {
+    font-family: var(--sb-normal-font);
+    
+    color: var(--sb-dark-color);
+}
+
+.content:deep(p) {
+    margin: 0;
+    margin-top: var(--sb-smaller-spacing);
+}
+
+.content:deep(p):first-child {
+    margin-top: 0;
+} 
+
+.content:deep(b) {
+    color: rgb(var(--pt-color));
+}
+
+.content:deep(u) {
+    color: rgb(var(--pt-color));
+    text-decoration-color: rgb(var(--pt-color));
+}
+</style>
